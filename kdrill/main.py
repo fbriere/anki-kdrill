4b529818a6f9fb6c6c01db3d402effeee5df7dd9
Frédéric Brière <fbriere@fbriere.net>
2012-11-19 20:59:05 -0500

    Moved all of deck.py into main.py

--- kdrill/main.py
+++ kdrill/main.py
@@ -2,7 +2,7 @@
 #
 # kdrill - Anki plugin to tag cards according to KDrill usefile
 #
-# Copyright (c) 2010  Frédéric Brière <fbriere@fbriere.net>
+# Copyright (c) 2012  Frédéric Brière <fbriere@fbriere.net>
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -18,11 +18,42 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import os.path
+from anki.cards import Card
+from anki.facts import Fact, Field
 
-from kdrill.deck import splitDeck
+from sqlalchemy.orm import eagerload_all
+
 from kdrill.usefile import parse_usefile
 
+
+def getCards(deck, templates_id):
+    """Fetch all cards beloning to some templates."""
+    return deck.s.query(Card).\
+            options(eagerload_all(Card.fact, Fact.fields, Field.fieldModel)).\
+            filter(Card.cardModelId.in_(templates_id)).all()
+
+def splitCards(deck, cards, field_name, values):
+    """Split a set of cards in two: those matching the values set, and those
+    that don't.
+    """
+    found = []
+    not_found = []
+
+    for card in cards:
+        if card.fact[field_name] in values:
+            found.append(card.id)
+        else:
+            not_found.append(card.id)
+
+    return found, not_found
+
+def splitDeck(deck, field, values, templates):
+    """Split a deck in two: cards matching the values set, and those that
+    don't.
+    """
+    templates_id = map(lambda t: t.id, templates)
+    cards = getCards(deck, templates_id)
+    return splitCards(deck, cards, field.name, values)
 
 def processDeck(deck, usefile_name, field, templates):
     """Tag the cards in a deck matching the kanji set."""
